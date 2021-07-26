b7fe97676223f59910d97257738c2b8cfbe72254
getssh <gtamrat33@gmail.com>
2021-07-26 11:29:06 +0300

    first printf sample

---
+++ holberton.h
@@ -0,0 +1,11 @@
+#ifndef _PRINTF
+#define _PRINTF
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <unistd.h>
+int _printf(const char *format, ...);
+int _putchar(char c);
+void _int(int);
+void _string(char *);
+#endif /* _PRINTF */
